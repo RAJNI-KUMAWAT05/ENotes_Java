eb785770207e765a13ca2accaeea6bfaffe26e6f
RAJNI-KUMAWAT05 <rajnidadarwal5@gmail.com>
2026-05-30 17:39:52 +0530

    Refactor DBConnect for new database connection
    Updated database connection details and improved connection handling.

--- src/main/java/com/Db/DBConnect.java
+++ src/main/java/com/Db/DBConnect.java
@@ -1,28 +1,43 @@
 package com.Db;
 
 import java.sql.Connection;
-
 import java.sql.DriverManager;
-import java.sql.SQLException;
-
 
 public class DBConnect {
-	private static Connection conn;
-	public static Connection getConn()
-	{
-		
-		try {
-			if(conn==null)
-			{
-				Class.forName("com.mysql.jdbc.Driver");
-				conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/enotes","root","root");
-			}
-		}
-		catch(Exception e) {
-		e.printStackTrace();
-		}
-		return conn;
-	}
 
+    private static Connection conn;
 
+    public static Connection getConn() {
+
+        try {
+
+            if (conn == null || conn.isClosed()) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                String url =
+                        "jdbc:mysql://zephyr.proxy.rlwy.net:36668/railway"
+                        + "?useSSL=false"
+                        + "&allowPublicKeyRetrieval=true"
+                        + "&serverTimezone=UTC";
+
+                String username = "root";
+
+                String password = "BuMbzRVmXGDcpyusaGmxntlenflNivhc";
+
+                conn = DriverManager.getConnection(
+                        url,
+                        username,
+                        password
+                );
+
+                System.out.println("Database Connected Successfully!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
 }
